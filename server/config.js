607ce510689e690e1bd1bbb2fee36580bb2df4be
Pavel Muller <muller.pav@gmail.com>
2018-08-17 21:54:31 -0300

    Fix host db bug

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.DB_URL || 'localhost',
+  host: process.env.DB_HOST || 'localhost',
   port: process.env.PORT || 5000,
   dbName: process.env.DB_NAME || 'meeting',
   dbUser: process.env.DB_USERNAME || 'root',
