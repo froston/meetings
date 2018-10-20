4ceb72438461ac34a95bb7e5f82043b7b2333e90
Pavel Muller <muller.pav@gmail.com>
2018-10-20 14:54:32 -0300

    Improve UX

--- client/src/components/WeekTab.js
+++ client/src/components/WeekTab.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
-import { Box, Card, Button, TextInput } from 'grommet'
-import { SaveIcon } from 'grommet/components/icons/base'
+import { Box, Tiles, Tile, Paragraph, Header, Heading, Menu, Anchor, TextInput, Button } from 'grommet'
+import { UserSettingsIcon, SaveIcon } from 'grommet/components/icons/base'
 import { consts } from '../utils'
 import { reading, initialCall, returnVisit, bibleStudy, talk } from '../images'
+import { TaskList } from '../containers'
 
 class WeekTab extends React.PureComponent {
   state = {
     updatting: {},
     task: {},
-    newPoint: null
+    newPoint: null,
+    taskForm: true,
+    student: {}
   }
   handleEdit = task => {
     this.setState({
@@ -25,6 +28,16 @@ class WeekTab extends React.PureComponent {
     if (newPoint > 0) {
       this.props.handleChangePoint(newPoint, task)
     }
+  }
+  showTasks = task => {
+    const student = {
+      id: task.student_id,
+      name: task.name
+    }
+    this.setState({
+      taskForm: false,
+      student
+    })
   }
   getImage = task => {
     let image
@@ -54,7 +67,7 @@ class WeekTab extends React.PureComponent {
     const { updatting, newPoint } = this.state
     let rvIndex = 0
     return (
-      <Box margin="small" direction="row" wrap>
+      <Tiles fill flush selectable className="week-tab">
         {consts.scheduleOptions.map(taskFullName => {
           const taskName = taskFullName.includes('Return Visit') ? taskFullName.substring(3) : taskFullName
           let mainTask
@@ -68,47 +81,76 @@ class WeekTab extends React.PureComponent {
             helperTask = tasks.find(t => t.task === taskName && t.helper)
           }
           if (mainTask) {
+            const className = mainTask.task && mainTask.task.split(' ').join('')
             return (
-              <Box key={mainTask.id} margin="small">
-                <Card
-                  thumbnail={this.getImage(mainTask.task)}
-                  label={t(`common:${mainTask.task}`)}
-                  heading={mainTask.name}
-                  textSize="small"
-                  link={<Button label={t('change')} primary onClick={() => handleChangeTask(mainTask)} />}
-                  description={
-                    <div style={{ margin: '10px 0' }}>
-                      <span style={{ visibility: helperTask ? '' : 'hidden' }}>
-                        <a onClick={() => handleChangeTask(helperTask, true)}>
-                          <b>{t('common:helper')}: </b> {helperTask && helperTask.name}
-                        </a>
+              <Tile
+                key={mainTask.id}
+                separator="top"
+                align="start"
+                basis="1/4"
+                style={{ position: 'relative' }}
+                className={className}
+              >
+                <Header size="small" pad={{ horizontal: 'small' }}>
+                  <Heading tag="h4" strong={true} margin="none">
+                    {t(`common:${mainTask.task}`)}
+                  </Heading>
+                </Header>
+                <Box pad="small">
+                  <Paragraph margin="none">
+                    <span>
+                      <b>{t('common:name')}: </b>
+                      <a onClick={() => this.showTasks(mainTask)}>{mainTask.name}</a>
+                    </span>
+                    <br />
+                    <span style={{ display: helperTask ? '' : 'none' }}>
+                      <b>{t('common:helper')}: </b>
+                      <a onClick={() => this.showTasks(helperTask)}>
+                        {helperTask && helperTask.name}
                         <br />
-                      </span>
-                      <span>
-                        {updatting[mainTask.id] ? (
-                          <div>
-                            <TextInput
-                              value={newPoint}
-                              onDOMChange={e => this.setState({ newPoint: e.target.value })}
-                            />
-                            <Button icon={<SaveIcon />} onClick={this.handleChangePoint} />
-                          </div>
-                        ) : (
-                          <a onClick={() => this.handleEdit(mainTask)}>
-                            <b>{t('common:point')}: </b> {mainTask.point}
-                          </a>
-                        )}
-                      </span>
-                    </div>
-                  }
+                      </a>
+                    </span>
+                    <span>
+                      <b>{t('common:point')}: </b>
+                      {updatting[mainTask.id] ? (
+                        <div>
+                          <TextInput value={newPoint} onDOMChange={e => this.setState({ newPoint: e.target.value })} />
+                          <Button icon={<SaveIcon />} onClick={this.handleChangePoint} />
+                        </div>
+                      ) : (
+                        <a onClick={() => this.handleEdit(mainTask)}>{mainTask.point}</a>
+                      )}
+                    </span>
+                  </Paragraph>
+                </Box>
+                <Menu
+                  responsive={true}
+                  icon={<UserSettingsIcon />}
+                  closeOnClick
+                  direction="row"
+                  size="small"
+                  style={{ padding: '10px 10px 0', position: 'absolute', right: 0, top: 0 }}
+                >
+                  <Anchor href="#" onClick={() => handleChangeTask(mainTask)}>
+                    {t('change')}
+                  </Anchor>
+                  <Anchor href="#" onClick={() => handleChangeTask(helperTask, true)}>
+                    {t('changeHelper')}
+                  </Anchor>
+                </Menu>
+                <TaskList
+                  hidden={this.state.taskForm}
+                  student={this.state.student}
+                  handleClose={() => this.setState({ taskForm: true })}
+                  showForm={false}
                 />
-              </Box>
+              </Tile>
             )
           } else {
             return null
           }
         })}
-      </Box>
+      </Tiles>
     )
   }
 }
